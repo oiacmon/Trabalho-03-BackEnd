f8ce97017149affd3673140d0f99c83241e4a42c
Oiacmon <caiodornellas@outlook.com>
2026-04-14 12:19:52 -0300

    Classe Reprodução

---
+++ src/main/java/backend/trabalho03/model/Reproducao.java
@@ -0,0 +1,27 @@
+package backend.trabalho03.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Reproducao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "peixe_pai_id")
+    private Peixe peixePai;
+
+    @ManyToOne
+    @JoinColumn(name = "peixe_mae_id")
+    private Peixe peixeMae;
+
+    private LocalDate dataReproducao;
+    private Integer quantidadeFilhotes;
+
+    public Reproducao() {}
+
+}
+
