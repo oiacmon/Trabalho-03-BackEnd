95b0a209803cbb1608f877e939740412b8d29251
Oiacmon <caiodornellas@outlook.com>
2026-04-14 11:28:34 -0300

    feat: Configurações de Ambiente

---
+++ src/main/java/backend/trabalho03/Trabalho03Application.java
@@ -0,0 +1,13 @@
+package backend.trabalho03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalho03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabalho03Application.class, args);
+	}
+
+}
---
+++ src/test/java/backend/trabalho03/Trabalho03ApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.trabalho03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabalho03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
